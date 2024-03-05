c12301934486d57e8cbe9c8ebd3249ea6ddb159d
Leonid <44239176+robert-werner@users.noreply.github.com>
2024-03-05 14:44:44 +0300

    Update get_raster_fragment.py
    Update get_raster_fragment to osgeo,gdal version

--- get_raster_fragment/get_raster_fragment.py
+++ get_raster_fragment/get_raster_fragment.py
@@ -4,19 +4,16 @@ def get_raster_fragment(input_raster_path, output_raster_path, bounds):
     if any(isinstance(item, str) for item in bounds):
         raise ValueError("Bounds must be 4 float elements")
     print("BBox is valid, driving through...")
-    bbox = box(*bounds)
-    geometry = mapping(bbox)
-    with rasterio.open(input_raster_path) as src:
-        print("Reading raster...")
-        out_image, out_transform = mask(src, [geometry], crop=True)
-        out_meta = src.meta.copy()
-        out_meta.update({
-            "driver": "COG",
-            "height": out_image.shape[1],
-            "width": out_image.shape[2],
-            "transform": out_transform
-        })
-        print("Writing to {}".format(output_raster_path))
-        with rasterio.open(output_raster_path, "w", **out_meta) as dest:
-            dest.write(out_image)
+    minx, miny, maxx, maxy = bounds[0], bounds[1], bounds[2], bounds[3]
+    print("Reading raster...")
+    src_ds = gdal.Open(input_raster_path)
+    translate_options = gdal.TranslateOptions(
+        projWin=[minx, maxy, maxx, miny],
+        format='COG',
+        outputType=gdal.GDT_Unknown
+    )
+    print("Writing to {}".format(output_raster_path))
+    gdal.Translate(output_raster_path, src_ds, options=translate_options)
+    src_ds = None
+    print("Finished")
     return output_raster_path
