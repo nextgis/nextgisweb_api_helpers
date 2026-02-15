b5a53c3263dbf8e6e4db36fd95bba2e52eab34fc
Maxim Dubinin <maxim.dubinin@nextgis.com>
2026-02-15 13:54:58 +0300

    better description

--- add_layer_to_webmap/add_layer_to_webmap.py
+++ add_layer_to_webmap/add_layer_to_webmap.py
@@ -1,13 +1,16 @@
-# Add a layer (style) to an existing Web map, keeping it's content intact
-# Prerequisits (use sandbox.nextgis.com to try): 
+# Add a layer (style) to an existing Web map
+# Try on sandbox.nextgis.com
+# Prerequisites: 
 # - known web map id
 # - known layer style id
+# Read more: https://sandbox.nextgis.com/doc/api#/Resource/put_api_resource__id_
 
 import requests
 
 ngw_host = 'https://sandbox.nextgis.com'
 auth = ('administrator','demodemo')
 webmap_id = 5
+layer_style_id = 12
 api_url = ngw_host + '/api/resource/' + str(webmap_id)
 
 # Get Web map contents
@@ -17,11 +20,11 @@ webmap_json = resp.json()['webmap']
 # Add a new Web map layer (i.e. style)
 new_layer = {
     "item_type": "layer",
-    "display_name": "madcity2",
+    "display_name": "madcity",
     "layer_enabled": True,
     "layer_identifiable": True,
     "layer_adapter": "image",
-    "layer_style_id": 12,
+    "layer_style_id": layer_style_id,
 }
 webmap_json["root_item"]["children"].append(new_layer)
 payload = {"webmap": webmap_json}
