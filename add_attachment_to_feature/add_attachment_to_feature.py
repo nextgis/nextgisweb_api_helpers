f624e8b051d686781ab75af9b7a3c7b23edc5368
Maxim Dubinin <maxim.dubinin@nextgis.com>
2026-06-08 12:19:04 +0300

    update add attachment example

--- add_attachment_to_feature/add_attachment_to_feature.py
+++ add_attachment_to_feature/add_attachment_to_feature.py
@@ -1,5 +1,6 @@
-import requests
 import json
+
+import requests
 
 ngw_host = 'https://sandbox.nextgis.com'
 auth = ('administrator','demodemo')
@@ -10,16 +11,20 @@ attachment_filename = 'PA190477_ShiftN_crop.jpg'
 if __name__ == '__main__':
     with open(attachment_filename, 'rb') as f:
         #upload attachment to NGW
-        response = requests.put(ngw_host + '/api/component/file_upload/upload', data=f, auth=auth)
+        response = requests.put(ngw_host + '/api/component/file_upload/', data=f, auth=auth)
+        response.raise_for_status()
         json_data = response.json()
-        json_data['filename'] = attachment_filename
+        json_data['name'] = attachment_filename
 
     attach_data = {}
     attach_data['file_upload'] = json_data
 
     #add attachment to a feature
     post_url = ngw_host + '/api/resource/' + str(vector_layer_id) +'/feature/' + str(feature_id) + '/attachment/'
+    response.raise_for_status()
     response = requests.post(post_url, data=json.dumps(attach_data), auth=auth)
     
     if response.status_code != 200:
         print(response.text)
+    else:
+        print("Success. See result here: https://sandbox.nextgis.com/resource/11/feature/1")
